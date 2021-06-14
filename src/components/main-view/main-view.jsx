8edb0ed8738f5b2b9fcd799e8be9f75c7337e490
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-13 22:30:26 -0500

    Added descriptions and image paths to movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,9 +9,9 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+        { _id: 1, Title: 'Inception', Description: 'Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people’s dreams and steal their secrets from their subconscious. His skill has made him a hot commodity in the world of corporate espionage but has also cost him everything he loves. Cobb gets a chance at redemption when he is offered a seemingly impossible task: Plant an idea in someone’s mind. If he succeeds, it will be the perfect crime, but a dangerous enemy anticipates Cobb’s every move.', ImagePath: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/inceptionmovie-newposter-big.jpg?itok=zn3rPAOq'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Andy Dufresne is sentenced to two consecutive life terms in prison for the murders of his wife and her lover and is sentenced to a tough prison. However, only Andy knows he didn\'t commit the crimes. While there, he forms a friendship with Red, experiences brutality of prison life, adapts, helps the warden, etc., all in 19 years.', ImagePath: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/shaw-poster.jpg?itok=RQn-PjG5'},
+        { _id: 3, Title: 'The Dark Knight', Description: 'With the help of allies Lt. Jim Gordon and DA Harvey Dent, Batman has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism.', ImagePath: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/the-dark-knight-pi.jpg?itok=Tspp08pQ'}
       ],
       selectedMovie: null
     };
