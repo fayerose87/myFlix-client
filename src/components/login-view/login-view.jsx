1c276eddee12c35d5208dfdd1e1cb186eb549d6e
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-16 19:34:30 -0500

    Created login-view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('')
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the servcer for authentication */
+    /* then call props.onLoggedIn(username) */
+    //props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input tyupe="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
