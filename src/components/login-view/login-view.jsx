6f1ee0c9324a5d66b059eef19e61f4ff3f7d90eb
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-17 21:26:55 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -26,3 +27,7 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.propTypes = {
+  user: propTypes.func.isRequired
+};
