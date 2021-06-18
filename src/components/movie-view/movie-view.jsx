8878ae238692a453df62a2de151e685e6ab9916c
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-17 21:02:12 -0500

    Added PropTypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
 
@@ -32,3 +33,16 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired}),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired})
+   }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+ };
