82d95814f0930f00db9d5e1d20713cf6e8d24056
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-21 20:02:56 -0500

    Install react bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
+import Navbar from 'react-bootstrap/Navbar'
 
 // Import statement to indicate that you need to bundle `./index.scss';
 import './index.scss';
@@ -9,7 +11,9 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     );
   }
 }
