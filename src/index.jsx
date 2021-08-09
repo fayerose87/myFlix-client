cc37cb748cc7ca8f61624c36fb32421045e01630
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-08-08 21:09:58 -0500

    Prettier fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import moviesApp from './reducers/reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import React from "react";
+import ReactDOM from "react-dom";
+import Container from "react-bootstrap/Container";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesApp from "./reducers/reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-import MainView from './components/main-view/main-view';
+import MainView from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss';
-import './index.scss';
+import "./index.scss";
 
 const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return(
-     <Provider store={store}>
+    return (
+      <Provider store={store}>
         <Container>
           <MainView />
         </Container>
@@ -26,9 +26,8 @@ class MyFlixApplication extends React.Component {
   }
 }
 
-
 // Finds the root of app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 // Tells React to render app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
